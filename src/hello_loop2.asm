0d20c0e6448e2e5f869a2e51070cf6a3c2afbd5e
mario <mt3000@gmx.de>
2021-03-07 10:28:50 -0800

    Added memory

---
+++ src/hello_loop2.asm
@@ -0,0 +1,22 @@
+.data
+    msg: .asciz "Hello, World\n"
+
+.text
+.globl main
+
+main:
+    movq $10, %rcx      # number of iterations
+    decq %rcx           # decrement because we test for zero so n - 1
+L0:
+    pushq %rcx          # preserve rcx
+    movq $1, %rax       # 1 = write
+    movq $1, %rdi       # 1 = to stdout
+    movq $msg, %rsi     # string ptr
+    movq $13, %rdx      # length of string, without null terminator
+    syscall             # call kernel
+    popq %rcx           # restore rcx
+    loop L0
+L1:
+    movq $60, %rax      # 60 = exit
+    xorl %edi, %edi     # 0 = exit code for success
+    syscall             # call kernel
---
+++ src/memory.asm
@@ -0,0 +1,46 @@
+.extern printf
+
+.data
+    x: .quad 0xABC
+    fmt: .asciz "X = %llx\n"
+
+.text
+.globl main
+
+print:
+    pushq %rbp
+    movq %rsp, %rbp
+
+    movq $fmt, %rdi
+    movq (x), %rsi
+    xorl %eax, %eax
+    callq printf
+
+    movq %rbp, %rsp
+    popq %rbp
+    retq
+
+main:
+    pushq %rbp
+    movq %rsp, %rbp
+
+    callq print
+
+    movq $0xFFFF, (x)
+    callq print
+
+    leaq (x), %rax
+    movq %rax, (x)
+    callq print
+
+    movq $5, (x)
+    movq (x), %rax
+    decq %rax
+    movq %rax, (x)
+    callq print
+
+    movq $print, %rax
+    callq *%rax
+
+    movq %rbp, %rsp
+    popq %rbp
